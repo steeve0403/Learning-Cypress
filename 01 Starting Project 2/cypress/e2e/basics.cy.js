1eb38d1d914da2e7d8ab8b8549eb100b4a3c386d
steeve0304 <zychsteeve4@gmail.com>
2023-12-13 14:12:04 +0100

    add first test cypress

--- 01 Starting Project 2/cypress/e2e/basics.cy.js
+++ 01 Starting Project 2/cypress/e2e/basics.cy.js
@@ -1,5 +1,8 @@
+/// <reference types="Cypress" /> /* allows us to leverage VS Code Intellisense for the autocompletion */
+
 describe('task page', () => {
   it('should render the main image', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('http://localhost:5173/');
+    cy.get();
+  });
+});
