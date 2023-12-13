0d35a2f3e765b35b48246b4851e0157fada68284
steeve0304 <zychsteeve4@gmail.com>
2023-12-13 15:01:04 +0100

    add files tasks

--- 01 Starting Project 2/cypress/e2e/tasks.cy.js
+++ 01 Starting Project 2/cypress/e2e/tasks.cy.js
@@ -2,5 +2,6 @@ describe('tasks management', () => {
     it('should open and close the new task modal', () => {
         cy.visit('http://localhost:5173/');
         cy.contains('Add Task').click();
+        cy.get('.backdrop').click({force: true});
     })
 });
