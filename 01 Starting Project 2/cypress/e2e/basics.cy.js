8f49b60cc530cd77f9113b672b96d2f4e0f3d922
steeve0304 <zychsteeve4@gmail.com>
2023-12-13 14:35:27 +0100

    use class and id selector on dom for cypress

--- 01 Starting Project 2/cypress/e2e/basics.cy.js
+++ 01 Starting Project 2/cypress/e2e/basics.cy.js
@@ -5,4 +5,11 @@ describe('task page', () => {
     cy.visit('http://localhost:5173/');
     cy.get('.main-header img');
   });
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('h1').should('have.length', 1);
+    cy.get('h1').contains('My Cypress Course Tasks');
+    // cy.contains('My Cypress Course Tasks')
+  });
 });
