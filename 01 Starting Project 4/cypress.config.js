c7d365fc5851370324e402dccd8cdbbaabc7026f
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 17:01:14 +0100

    Configuring the baseURL

--- 01 Starting Project 4/cypress.config.js
+++ 01 Starting Project 4/cypress.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     // Customize screenshot settings by setting screenshotOnRunFailure.
 
   e2e: {
+    baseUrl: 'http://localhost:5173', // Setting the URL base simplifies calling the visit function, allowing you to focus on the URL path rather than the full URL.
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- 01 Starting Project 4/cypress/e2e/contact.cy.js
+++ 01 Starting Project 4/cypress/e2e/contact.cy.js
@@ -2,7 +2,7 @@
 
 describe('contact form', () => {
   it('should submit the form', () => {
-    cy.visit('http://localhost:5173/about');
+    cy.visit('/about');
     cy.get('[data-cy="contact-input-message"]').type('Hello world!');
     cy.get('[data-cy="contact-input-name"]').type('John Doe');
     cy.get('[data-cy="contact-btn-submit"]').then((el) => {
@@ -22,7 +22,7 @@ describe('contact form', () => {
   });
 
   it('should validate the form input', () => {
-    cy.visit('http://localhost:5173/about');
+    cy.visit('/about');
     cy.get('[data-cy="contact-btn-submit"]').click();
     cy.get('[data-cy="contact-btn-submit"]').then((el) => {
       expect(el).to.not.have.attr('disabled');
