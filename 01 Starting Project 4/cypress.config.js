3039f3cbcbd463408bcf23466cf3829939e08bf3
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 16:43:44 +0100

    The Global Config File

--- 01 Starting Project 4/cypress.config.js
+++ 01 Starting Project 4/cypress.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // video: true,
+    // Control video creation when running tests with Cypress run.
+  //screenshotOnRunFailure: true;
+    // Customize screenshot settings by setting screenshotOnRunFailure.
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
