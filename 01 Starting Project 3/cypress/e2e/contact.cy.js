9f25de9396c97cd8e718e646d0da81fbc05190ae
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 11:34:27 +0100

    Working with Values & Aliases

--- 01 Starting Project 3/cypress/e2e/contact.cy.js
+++ 01 Starting Project 3/cypress/e2e/contact.cy.js
@@ -9,8 +9,9 @@ describe('contact form', () => {
         cy.get('[data-cy="contact-btn-submit"]')
             .contains('Send Message')
             .should('not.have.attr', 'disabled');
-        cy.get('[data-cy="contact-btn-submit"]').click();
-        cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
-        cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+        cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+        cy.get('@submitBtn').click();
+        cy.get('@submitBtn').contains('Sending...');
+        cy.get('@submitBtn').should('have.attr', 'disabled');
     });
 });
