e2bb364b78ae448277e2e13f9aa66a71eb80d67e
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 10:58:51 +0100

    Testing Page Navigation

--- 01 Starting Project 3/cypress/e2e/navigation.cy.js
+++ 01 Starting Project 3/cypress/e2e/navigation.cy.js
@@ -3,6 +3,12 @@
 describe('page navigation', () => {
     it('should navigate between pages', () => {
         cy.visit('http://localhost:5173/');
-        cy.get('[data-cy="header-about-link"]');
+        cy.get('[data-cy="header-about-link"]').click();
+        cy.location('pathname').should('eq', '/about'); // /about => About page
+        cy.go('back');
+        cy.location('pathname').should('eq', '/'); // => Home page
+        cy.get('[data-cy="header-about-link"]').click();
+        cy.get('[data-cy="header-home-link"]').click();
+        cy.location('pathname').should('eq', '/'); // => Home page
     });
 });
