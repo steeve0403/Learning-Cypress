6497b66d940eb4171058537ddf8ad368faf84bb2
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 10:32:48 +0100

    Selecting Elements - The Best Practice Way

---
+++ 01 Starting Project 3/cypress/e2e/navigation.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe('page navigation', () => {
+    it('should navigate between pages', () => {
+        cy.visit('http://localhost:5173/');
+        cy.get('[data-cy="header-about-link"]');
+    });
+});
