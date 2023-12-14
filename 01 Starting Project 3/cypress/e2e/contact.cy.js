d417c96038d3d4b4cd214579bbb892de61c819ed
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 14:44:46 +0100

    Losing & Gaining Focus

--- 01 Starting Project 3/cypress/e2e/contact.cy.js
+++ 01 Starting Project 3/cypress/e2e/contact.cy.js
@@ -23,9 +23,29 @@ describe('contact form', () => {
         cy.visit('http://localhost:5173/about');
         cy.get('[data-cy="contact-btn-submit"]').click();
         cy.get('[data-cy="contact-btn-submit"]').then(el => {
-            expect(el.attr('disabled')).to.be.undefined;
+            expect(el).to.not.have.attr('disabled');
             expect(el.text()).to.not.equal('Sending...');
         });
         cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+        cy.get('[data-cy="contact-input-message"]').focus().blur();
+        cy.get('[data-cy="contact-input-message"]')
+            .parent()
+            .then((el) => {
+                expect(el.attr('class')).to.contains('invalid');
+            });
+
+        cy.get('[data-cy="contact-input-name"]').focus().blur();
+        cy.get('[data-cy="contact-input-name"]')
+            .parent()
+            .then((el) => {
+                expect(el.attr('class')).to.contains('invalid');
+            });
+
+        cy.get('[data-cy="contact-input-email"]').focus().blur();
+        cy.get('[data-cy="contact-input-email"]')
+            .parent()
+            .then((el) => {
+                expect(el.attr('class')).to.contains('invalid');
+            });
     });
 });
