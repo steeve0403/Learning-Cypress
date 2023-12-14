29374005bad1cf5b6a1ebfe6173008d53dd93444
steeve0304 <zychsteeve4@gmail.com>
2023-12-14 11:28:36 +0100

    Chaining commands

--- 01 Starting Project 3/cypress/e2e/contact.cy.js
+++ 01 Starting Project 3/cypress/e2e/contact.cy.js
@@ -6,8 +6,9 @@ describe('contact form', () => {
         cy.get('[data-cy="contact-input-message"]').type('Hello World');
         cy.get('[data-cy="contact-input-name"]').type('John Doe');
         cy.get('[data-cy="contact-input-email"]').type('test@example.com');
-        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
-        cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
+        cy.get('[data-cy="contact-btn-submit"]')
+            .contains('Send Message')
+            .should('not.have.attr', 'disabled');
         cy.get('[data-cy="contact-btn-submit"]').click();
         cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
         cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
