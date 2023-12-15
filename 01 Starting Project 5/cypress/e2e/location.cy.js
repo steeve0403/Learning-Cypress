8bd0eb23431bf1c3762abbae2144627610102010
steeve0304 <zychsteeve4@gmail.com>
2023-12-15 14:31:06 +0100

    Adding a Fake Stub Implementation

--- 01 Starting Project 5/cypress/e2e/location.cy.js
+++ 01 Starting Project 5/cypress/e2e/location.cy.js
@@ -5,8 +5,18 @@ describe('share location', () => {
   it('should fetch the user location', () => {
     cy.visit('/').then((win) => {
       cy.stub(win.navigator.geolocation, 'getCurrentPosition').as( // The Cypress stub method replaces an existing method.
-          'getUserPosition'); // When replacing with an empty function, the callback-dependent user interface (UI) update does not occur.
-    });
+          'getUserPosition' // When replacing with an empty function, the callback-dependent user interface (UI) update does not occur.
+      ).callsFake((cb) => { // Using callsFake to override the default implementation of getCurrentPosition.
+        setTimeout(() => {
+          cb({
+            coords: {
+              latitude: 37.5,
+              longitude: 48.01,
+            },
+          });
+        }, 100); //  Added delay with setTimeout to simulate load time.
+      });
+    }); // Successful testing after dummy implementation of getCurrentPosition
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
