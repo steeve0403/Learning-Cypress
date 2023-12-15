c1ba23dbb29ec306d85fb93ac7838809de506bba
steeve0304 <zychsteeve4@gmail.com>
2023-12-15 15:46:36 +0100

    Using Fixtures

--- 01 Starting Project 5/cypress/e2e/location.cy.js
+++ 01 Starting Project 5/cypress/e2e/location.cy.js
@@ -3,19 +3,17 @@
 // The goal is not to test whether the browser is recovering the location correctly, but rather whether the application is using the data correctly.
 describe('share location', () => {
   beforeEach(() => { //  Using a hook (beforeEach) to reuse a stub between tests.
+    cy.fixture('user-location.json').as('userLocation'); //  Using cy.fixture to load and access data.
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as( // The Cypress stub method replaces an existing method.
-          'getUserPosition' // When replacing with an empty function, the callback-dependent user interface (UI) update does not occur.
-      ).callsFake((cb) => { // Using callsFake to override the default implementation of getCurrentPosition.
-        setTimeout(() => {
-          cb({
-            coords: {
-              latitude: 37.5,
-              longitude: 48.01,
-            },
-          });
-        }, 100); //  Added delay with setTimeout to simulate load time.
-      });
+      cy.get('@userLocation').then(fakePosition => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition').as( // The Cypress stub method replaces an existing method.
+            'getUserPosition' // When replacing with an empty function, the callback-dependent user interface (UI) update does not occur.
+        ).callsFake((cb) => { // Using callsFake to override the default implementation of getCurrentPosition.
+          setTimeout(() => {
+            cb(fakePosition);
+          }, 100); //  Added delay with setTimeout to simulate load time.
+        });
+      })
       cy.stub(win.navigator.clipboard, 'writeText')
           .as('saveToClipboard')
           .resolves();
@@ -33,9 +31,12 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get('@saveToClipboard').should('have.been.called'); // Use the assertion should('have.been.calledWithMatch') to check arguments passed to the dummy function.
-    cy.get('@saveToClipboard').should(
-        'have.been.calledWithMatch',
-        new RegExp(`${37.5}.*${48.01}.*${encodeURI('John Doe')}`) // URL encoding to match the required format when checking arguments.
-    );
+    cy.get('@userLocation').then(fakePosition => {
+      const { latitude, longitude } = fakePosition.coords;
+      cy.get('@saveToClipboard').should(
+          'have.been.calledWithMatch',
+          new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`) // URL encoding to match the required format when checking arguments.
+      );
+    });
   });
 });
