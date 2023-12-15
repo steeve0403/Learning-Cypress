3a1e97956d63acdc8f95c0023a3e1ef7f799b8de
steeve0304 <zychsteeve4@gmail.com>
2023-12-15 14:01:58 +0100

    Creating & Using a Stub

--- 01 Starting Project 5/cypress/e2e/location.cy.js
+++ 01 Starting Project 5/cypress/e2e/location.cy.js
@@ -1,8 +1,12 @@
 /// <reference types="cypress" />
 
+// The goal is not to test whether the browser is recovering the location correctly, but rather whether the application is using the data correctly.
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then((win) => {
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition'); // The Cypress stub method replaces an existing method.
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called');
   });
 });
