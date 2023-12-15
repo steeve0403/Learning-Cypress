75bc9022260fbabed4ab0d34c5472867e3c63203
steeve0304 <zychsteeve4@gmail.com>
2023-12-15 14:12:28 +0100

    When Empty Stubs Are Not Enough

--- 01 Starting Project 5/cypress/e2e/location.cy.js
+++ 01 Starting Project 5/cypress/e2e/location.cy.js
@@ -4,9 +4,12 @@
 describe('share location', () => {
   it('should fetch the user location', () => {
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition'); // The Cypress stub method replaces an existing method.
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as(
+          'getUserPosition'); // The Cypress stub method replaces an existing method.
     });
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain', 'Location fetched'); // contains()
   });
 });
