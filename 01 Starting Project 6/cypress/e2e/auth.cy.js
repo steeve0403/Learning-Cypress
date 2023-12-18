da2c709f88d0b2101dea2283eef987540cc88dce
steeve0304 <zychsteeve4@gmail.com>
2023-12-18 11:08:09 +0100

    Adding a "Login" Test

--- 01 Starting Project 6/cypress/e2e/auth.cy.js
+++ 01 Starting Project 6/cypress/e2e/auth.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="Cypress" />
 
 describe('Auth', () => {
-    beforeEach(() => { // It is important to use beforeEach to reset the database before each test.
+    beforeEach(() => { // It's important to use beforeEach to reset the database before each test.
         cy.task('seedDatabase');
     })
+
     it('should signup', () => {
         cy.visit('/signup');
         cy.get('[data-cy="auth-email"]').click();
@@ -14,6 +15,16 @@ describe('Auth', () => {
         cy.getCookie('__session').its('value').should('not.be.empty');
         // Session cookie validation (__session) with a non-empty value.
         // Using the "its" method to inspect the properties of an object (ex cookie value).
+    });
+
+    it('should login', () => {
+        cy.visit('/login');
+        cy.get('[data-cy="auth-email"]').click();
+        cy.get('[data-cy="auth-email"]').type('test@example.com');
+        cy.get('[data-cy="auth-password"]').type('testpassword');
+        cy.get('[data-cy="auth-submit"]').click();
+        cy.location('pathname').should('eq', '/takeaways');
+        cy.getCookie('__session').its('value').should('not.be.empty');
 
     });
 });
--- 01 Starting Project 6/cypress/e2e/newsletter.cy.js
+++ 01 Starting Project 6/cypress/e2e/newsletter.cy.js
@@ -4,7 +4,11 @@ describe('Newsletter', () => {
     beforeEach(() => {
         cy.task('seedDatabase');
     });
+
     it('should display a success message', () => {
+        // Using cy.intercept, it is possible to intercept and block outgoing HTTP requests, thus avoiding actually sending them to the backend.
+        // The interceptor configuration includes the definition of the URL to monitor and the type of request to block, such as POST requests to "/newsletter".
+        //  Adding an argument to the interceptor blocks the request and provides dummy data in response, speeding up testing.
         cy.intercept('POST','/newsletter*', {status: 201}).as('subscribe'); // intercept any HTTP request localhost:3000/newsletter?anything
         cy.visit('/');
         cy.get('[data-cy="newsletter-email"]').type('test@example.com');
@@ -12,9 +16,6 @@ describe('Newsletter', () => {
         cy.wait('@subscribe'); // Using aliasing with cy.wait makes it possible to specifically observe and wait for the interception of a query, ensuring a sequential execution of the tests.
         cy.contains('Thanks for signing up');
     });
-// Using cy.intercept, it is possible to intercept and block outgoing HTTP requests, thus avoiding actually sending them to the backend.
-// The interceptor configuration includes the definition of the URL to monitor and the type of request to block, such as POST requests to "/newsletter".
-//  Adding an argument to the interceptor blocks the request and provides dummy data in response, speeding up testing.
 
     it('should display validation errors', () => {
         cy.intercept('POST','/newsletter*', {message: 'Email exists already.'}).as('subscribe'); // Use the concept of interceptor to avoid the actual HTTP request and test the behavior of the application.
