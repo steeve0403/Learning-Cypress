f7637103997a486dd403ff0d2bb54790d1f1b192
steeve0304 <zychsteeve4@gmail.com>
2023-12-18 10:01:31 +0100

    More Interceptors

--- 01 Starting Project 6/cypress/e2e/newsletter.cy.js
+++ 01 Starting Project 6/cypress/e2e/newsletter.cy.js
@@ -12,11 +12,20 @@ describe('Newsletter', () => {
         cy.wait('@subscribe'); // Using aliasing with cy.wait makes it possible to specifically observe and wait for the interception of a query, ensuring a sequential execution of the tests.
         cy.contains('Thanks for signing up');
     });
-});
-
 // Using cy.intercept, it is possible to intercept and block outgoing HTTP requests, thus avoiding actually sending them to the backend.
 // The interceptor configuration includes the definition of the URL to monitor and the type of request to block, such as POST requests to "/newsletter".
 //  Adding an argument to the interceptor blocks the request and provides dummy data in response, speeding up testing.
+
+    it('should display validation errors', () => {
+        cy.intercept('POST','/newsletter*', {message: 'Email exists already.'}).as('subscribe');
+        cy.visit('/');
+        cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+        cy.get('[data-cy="newsletter-submit"]').click();
+        cy.wait('@subscribe');
+        cy.contains('Email exists already.');
+    });
+});
+
 
 
 
